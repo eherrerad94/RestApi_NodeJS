0a30abb2a72dd8601d5a092397cad83a7331677a
Eduardo Herrrera <laloherreradominguez@gmail.com>
2016-11-13 15:12:53 -0600

    corrigiendo metodo post

--- models/user.js
+++ models/user.js
@@ -1,12 +1,12 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+var mongoose  = require("mongoose"); //Requerir mongodb
+var	Schema    = mongoose.Schema; //BD
 
 
 /*definicion de un modelo con sus atributos y su tipo de dato*/
 var userSchema =  new Schema({
-	name:   { type: String  },
-	password:    { type: Number  },
-	admin: { type: Boolean },
+	name:      { type: String },
+	password:  { type: String },
+	admin: 	   { type: Boolean }
 });
 
 module.exports = mongoose.model('User', userSchema); //exportar al app.js
