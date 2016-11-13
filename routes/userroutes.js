6bc994f0255ee8ea050a27934d337549e02d1713
Eduardo Herrrera <laloherreradominguez@gmail.com>
2016-11-13 15:13:25 -0600

    metodo login

--- routes/userroutes.js
+++ routes/userroutes.js
@@ -4,9 +4,9 @@ module.exports = function(app,Ctrl){
 
 	app.group('/users', (router) => 
 		{
-      router.get("/",Ctrl.findAllUsers);
+      		router.get("/",Ctrl.findAllUsers);
 			router.post("/",Ctrl.addUser);
-      router.post('/authenticate',Ctrl.authenticate);
+      		router.post('/login',Ctrl.authenticate);
 			router.get("/:id",Ctrl.findById);
 			router.put("/:id",Ctrl.updateUser)
 			router.delete("/:id",Ctrl.deleteUser);
